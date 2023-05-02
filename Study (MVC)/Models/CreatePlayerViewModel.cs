1988027e31ada769d600d7f93d1bc9ae2611c8ea
DanyaPe <126086257+DanyaPe@users.noreply.github.com>
2023-05-02 22:09:07 +0600

    Тестовый коммит

--- Study (MVC)/Models/CreatePlayerViewModel.cs
+++ Study (MVC)/Models/CreatePlayerViewModel.cs
@@ -4,8 +4,6 @@ namespace Study__MVC_.Models
 {
     public class CreatePlayerViewModel
     {
-        [Required(ErrorMessage = "Необходи ввести хоть какое-то имя")]
-        [MinLength(2, ErrorMessage = "Минимальная длина имени должна быть 2 символа")]
         public string Name { get; set; }
     }
 }
